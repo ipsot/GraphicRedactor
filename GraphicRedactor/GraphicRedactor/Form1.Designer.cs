2f692b9d1191105955c0bd882ea10cf870f13e53
ipsot <76401884+ipsot@users.noreply.github.com>
2022-08-10 13:14:31 +0300

    Add files via upload

---
+++ GraphicRedactor/GraphicRedactor/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace GraphicRedactor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.заднийФонToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.переднийФонToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.заднийФонToolStripMenuItem,
+            this.переднийФонToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "Color";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // заднийФонToolStripMenuItem
+            // 
+            this.заднийФонToolStripMenuItem.Name = "заднийФонToolStripMenuItem";
+            this.заднийФонToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.заднийФонToolStripMenuItem.Text = "ColorButton";
+            this.заднийФонToolStripMenuItem.Click += new System.EventHandler(this.заднийФонToolStripMenuItem_Click);
+            // 
+            // переднийФонToolStripMenuItem
+            // 
+            this.переднийФонToolStripMenuItem.Name = "переднийФонToolStripMenuItem";
+            this.переднийФонToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.переднийФонToolStripMenuItem.Text = "ColotTitle";
+            this.переднийФонToolStripMenuItem.Click += new System.EventHandler(this.переднийФонToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.ContextMenuStrip = this.contextMenuStrip1;
+            this.button1.Location = new System.Drawing.Point(2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 86);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Изменить цвет кнопки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(695, 435);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem заднийФонToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem переднийФонToolStripMenuItem;
+    }
+}
+
---
+++ GraphicRedactor/GraphicRedactor/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicRedactor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void переднийФонToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog clrDlg = new ColorDialog();
+            if (clrDlg.ShowDialog()==DialogResult.OK)
+            {
+                button1.ForeColor = clrDlg.Color;
+            }
+        }
+
+        private void заднийФонToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog clrDlg = new ColorDialog();
+            if (clrDlg.ShowDialog() == DialogResult.OK)
+            {
+                button1.BackColor = clrDlg.Color;
+            }
+        }
+    }
+}
